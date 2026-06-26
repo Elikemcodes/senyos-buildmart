bb986a10a30170464d877dedfd86de596bd048bd
Elikemcodes <attipoeelikem2@gmail.com>
2026-06-26 15:32:35 +0000

    Add files via upload

--- rolls.js
+++ rolls.js
@@ -1,19 +1,15 @@
 function searchProducts() {
+    let input = document.getElementById("searchBox").value.toLowerCase();
 
-  let input = document.getElementById("searchBox").value.toLowerCase();
+    let products = document.querySelectorAll(".card");
 
-  let products = document.querySelectorAll(".product");
+    products.forEach(function(product) {
+        let text = product.innerText.toLowerCase();
 
-  products.forEach(product => {
-
-    let text = product.innerText.toLowerCase();
-
-    if (text.includes(input)) {
-      product.style.display = "block";
-    } else {
-      product.style.display = "none";
-    }
-
-  });
-
+        if (text.includes(input)) {
+            product.style.display = "";
+        } else {
+            product.style.display = "none";
+        }
+    });
 }
